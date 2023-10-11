6b197b1bd67497a21c06c5992761212b502f1c57
Beckyshee <wanjirubecky.rw@gmail.com>
2023-10-11 13:35:50 +0300

    conditional logic, loops, operarors

---
+++ index.js
@@ -0,0 +1,35 @@
+/*let team = "ManU"
+let value = (team == "Arsenal") ? "Think, Again" : (team == "Chelsea") ? "Buy a bike" : (team == "ManCity") ?
+ "you are a legend" : "Get a life"
+console.log(value);*/
+
+/*let team = "ManU"
+if(team == "Arsenal"){
+    console.log("think again");
+}
+else if(team == "Chelsea"){
+    console.log("buy a bike");
+} else if(team == "ManCity"){
+    console.log("a legend");
+}
+
+else{
+    console.log("Get a life");
+}*/
+
+/*let hour = 10
+let minute = 45
+if (hour > 8 && minute == 45){
+    console.log(`the time is ${hour}:45`);
+}else{console.log("sth else");}*/
+
+/*let isMale = true
+console.log(!isMale);*/
+
+/*let middle_name;
+let last_name = "baraka";
+console.log(middle_name ?? last_name ?? "kellah");*/
+
+
+
+
