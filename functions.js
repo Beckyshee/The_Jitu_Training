4f204ab34b0f08893bb3dcb6cc08517e8aca9868
Beckyshee <wanjirubecky.rw@gmail.com>
2023-10-13 10:05:10 +0300

    to do's

---
+++ functions.js
@@ -0,0 +1,27 @@
+// function calculate(a, b){
+//     return a * b;
+// }
+// const product = calculate(7, 2);
+// console.log(product);
+
+
+//arrow functions
+const isAdult = (age)=>{
+    if(age>=18) return true;
+    return false;
+}
+let sharon_is_adult = isAdult(17.5);
+//console.log(sharon_is_adult);
+
+
+//immediatly invoked function
+// (function(){
+//     console.log("IIF from an anyonymous function");
+// }) ();
+
+// (()=>{
+//     console.log("IIF from an anyonymous function");
+// }) ();
+
+
+
